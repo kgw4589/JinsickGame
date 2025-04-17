11d76bf7f0090e324aef704aa8c628c7d8a8392c
Yoimiya <kgw4589@gmail.com>
2025-04-17 09:34:16 +0900

    Fix: 비주얼 스크립팅 오류 해결

--- Assets/@Resource/Scripts/HUD.cs
+++ Assets/@Resource/Scripts/HUD.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
